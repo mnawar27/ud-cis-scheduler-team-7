e3ea965d684d20890acaf6220a55cc4071a42bf2
Maisha Nawar <maishanawar27@gmail.com>
2021-10-12 21:06:29 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler
+                <p>Maisha Nawar</p>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
