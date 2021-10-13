68798ab87128867a26d6cf37a9e40a1e808b18c6
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-12 21:14:54 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                UD CIS Scheduler
                 <p>Jakob Wolf</p>
+                <p>Macy Beach</p>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
