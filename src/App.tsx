07add7431840c4926e696717b4dc4dc116c6a37f
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-14 10:25:53 -0400

    added UD CIS scheduler?

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App(): JSX.Element {
     return (
         <div className="App">
+            UD CIS Scheduler
             <header className="App-header">
 
                 <h1>CS degree plan</h1>
