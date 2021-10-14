c71d396bfd745799c42e075bdc4c00d216b4b78c
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-14 10:33:37 -0400

    added layout sketch file, now understand cis scheduler

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            UD CIS Scheduler
             <header className="App-header">
 
-                <h1>CS degree plan</h1>
+                <h1>UD CIS Scheduler</h1> 
 
             </header>
         </div>
