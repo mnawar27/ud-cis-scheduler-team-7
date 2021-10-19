3ed9421e39f9af182a0c0eab73ecc6cdd0441e6b
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-19 10:30:14 -0400

    added rows and columns, menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Components/Menu.css";
+import {Menu} from "./Components/Menu";
+import { Container, Row, Col} from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-
-                <h1>UD CIS Scheduler</h1> 
-
-            </header>
-        </div>
+        <Container className="App">
+            <Row>
+                <header className="App-header">
+                    <h1>UD CIS Scheduler</h1> 
+                </header>
+            </Row>
+            <Row>
+                <Col>
+                    <Menu />
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+            
     );
 }
 
---
+++ src/Components/Menu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Menu(): JSX.Element{
+    return(
+        <ul>
+            <li>CISC 108</li>
+            <li>CISC 181</li>
+        </ul>
+    );
+}
