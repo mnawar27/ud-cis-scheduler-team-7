04449a19aa7f0f3fe4c82c7d77ee234ed78eb62f
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-19 17:10:25 -0400

    trying to fix parentheses error

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export function Menu(): JSX.Element{
-    return(<div className={"menu"}>
+    return(<div className="menu">
         <ul>
             <li>CISC 108</li>
             <li>CISC 181</li>
