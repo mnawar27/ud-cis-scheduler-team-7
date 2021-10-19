5fdad1170697260853771c9db8341fe41552ed85
jakobw10 <jakobw@udel.edu>
2021-10-19 10:29:51 -0400

    Semester component using Course interface

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-
+import Course from "../Interfaces/Course"
 export function Semester(): JSX.Element{
+    const Cisc108 : Course ={
+        name:"Cisc 108",
+        credits:3,
+    }
+    const Cisc181 : Course ={
+        name:"Cisc181",
+        credits:3,
+        prereq:Cisc108,
+    }
     const year =1;
     const season="Fall";
     return (<div className={"semester"}>
         <div>Year {year} {season} Semester </div>
         <div>Courses:
         </div>
-        <p>Cisc 108    Credits:3</p>
-        <p>Cisc 181    Credits:3</p>
+        <p>{Cisc108.name}    Credits:{Cisc108.credits}</p>
+        <p>{Cisc181.name}    Credits:{Cisc181.credits}</p>
     </div>
     );
 }
