da8bf377bdc79bf9e134610b893ae27f4d21c9ef
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-19 17:24:29 -0400

    stil trying to fix parentheses issue

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export function Menu(): JSX.Element{
-    return(<div className="menu">
-        <ul>
-            <li>CISC 108</li>
-            <li>CISC 181</li>
-        </ul>
-    </div>
+    return(
+        <div className="menu">
+            <ul>
+                <li>CISC 108</li>
+                <li>CISC 181</li>
+            </ul>
+        </div>
     );
 }
--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -3,12 +3,13 @@ import React from "react";
 export function Semester(): JSX.Element{
     const year =1;
     const season="Fall";
-    return (<div className={"semester"}>
-        <div>Year {year} {season} Semester </div>
-        <div>Courses:
+    return (
+        <div className="semester">
+            <div>Year {year} {season} Semester </div>
+            <div>Courses:
+            </div>
+            <p>Cisc 108    Credits:3</p>
+            <p>Cisc 181    Credits:3</p>
         </div>
-        <p>Cisc 108    Credits:3</p>
-        <p>Cisc 181    Credits:3</p>
-    </div>
     );
 }
