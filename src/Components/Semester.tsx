9d48216ccb295febb53f3433caae8ca514d6b6c7
jakobw10 <jakobw@udel.edu>
2021-10-19 17:30:16 -0400

    fixed passed in values

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import Course from "../Interfaces/Course";
 export function Semester(props:{year :number,season:string,courses:Course[]}): JSX.Element{
-    const Cisc108 : Course ={
-        name:"Cisc 108",
-        credits:3,
-    };
-    const Cisc181 : Course ={
-        name:"Cisc181",
-        credits:3,
-        prereq:Cisc108,
-    };
     return (<div className={"semester"}>
         <div>Year {props.year} {props.season} Semester </div>
         <div>Courses:
         </div>
-        <p>{Cisc108.name}    Credits:{Cisc108.credits}</p>
-        <p>{Cisc181.name}    Credits:{Cisc181.credits}</p>
+        <p>{props.courses[0].name}    Credits:{props.courses[0].credits}</p>
+        <p>{props.courses[1].name}    Credits:{props.courses[1].credits}</p>
     </div>
     );
 }
