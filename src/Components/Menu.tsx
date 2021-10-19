d96114459cfdded2d674f483d4062bafd3cbd117
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-19 10:45:23 -0400

    added style changes

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export function Menu(): JSX.Element{
-    return(
+    return(<div className={"menu"}>
         <ul>
             <li>CISC 108</li>
             <li>CISC 181</li>
         </ul>
+    </div>
     );
 }
