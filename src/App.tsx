01d200bba1ba79d3026f9c446381924e38e0db5d
jakobw10 <jakobw@udel.edu>
2021-10-21 09:58:40 -0400

    fix linter error

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./Components/Welcome.css";
 import { Welcome } from "./Components/Welcome";
 import Course from "./Interfaces/Course";
 import { Menu } from "./Components/Menu";
+import React from "react"
 
 
 function App(): JSX.Element {
