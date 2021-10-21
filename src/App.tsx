09f05c2dbec51fac60ef2d262a4473ca4403efdb
jakobw10 <jakobw@udel.edu>
2021-10-21 09:55:43 -0400

    fix formatting for linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Components/Menu.css";
-import {Menu} from "./Components/Menu";
-import "./Components/Semester.css";
 import { Semester } from "./Components/Semester";
 import "./Components/Welcome.css";
 import { Welcome } from "./Components/Welcome";
 import Course from "./Interfaces/Course";
+import { Menu } from "./Components/Menu";
 
 
 function App(): JSX.Element {
-    const Cisc108 : Course ={
-        name:"Cisc 108",
-        credits:3,
+    const Cisc108: Course = {
+        name: "Cisc 108",
+        credits: 3,
     };
-    const Cisc181 : Course ={
-        name:"Cisc181",
-        credits:3,
-        prereq:Cisc108,
+    const Cisc181: Course = {
+        name: "Cisc181",
+        credits: 3,
+        prereq: Cisc108,
     };
-    const courses=[Cisc108,Cisc181];
+    const courses = [Cisc108, Cisc181];
     return (
         <div className="App">
             <Welcome />
-            <div className="container-fluid"> 
+            <div className="container-fluid">
                 <div className="row">
                     <header className="App-header">
-                        <h1>UD CIS Scheduler</h1> 
+                        <h1>UD CIS Scheduler</h1>
                     </header>
                 </div>
                 <div className="row">
@@ -35,17 +30,16 @@ function App(): JSX.Element {
                         <Menu />
                     </div>
                     <div className="col">
-                        <Semester year={1} season={"Fall"} courses={courses}/>
-                        <Semester year={2} season={"Fall"} courses={courses}/>
+                        <Semester year={1} season={"Fall"} courses={courses} />
+                        <Semester year={2} season={"Fall"} courses={courses} />
                     </div>
                     <div className="col">
-                        <Semester year={1} season={"Spring"} courses={courses}/>
-                        <Semester year={2} season={"Spring"} courses={courses}/>
-                  </div>
+                        <Semester year={1} season={"Spring"} courses={courses} />
+                        <Semester year={2} season={"Spring"} courses={courses} />
+                    </div>
                 </div>
             </div>
-        </div>   
-    );
+        </div>
+    )
 }
-
-export default App;
+export default App
--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Course from "../Interfaces/Course";
 export function Semester(props:{year :number,season:string,courses:Course[]}): JSX.Element{
-    return (<div className={"semester"}>
+    return <div className={"semester"}>
         <div>Year {props.year} {props.season} Semester </div>
         <div>Courses:
         </div>
         <p>{props.courses[0].name}    Credits:{props.courses[0].credits}</p>
         <p>{props.courses[1].name}    Credits:{props.courses[1].credits}</p>
     </div>
-    );
+    ;
 }
