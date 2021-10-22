14e860671c3e4a843dee0c39c6fbd2fb7679b2d7
jakobw10 <jakobw@udel.edu>
2021-10-22 13:01:30 -0400

    fixed linter error

--- src/Components/Course.tsx
+++ src/Components/Course.tsx
@@ -1,5 +1,6 @@
-import Course from "../Interfaces/Course"
-export function Course(props:{course:Course}){
+import Course from "../Interfaces/Course";
+import react from "react";
+export function Course(props:{course:Course}): JSX.Element{
     return(
         <div>{props.course.name}    Credits:{props.course.credits}</div>
     );
