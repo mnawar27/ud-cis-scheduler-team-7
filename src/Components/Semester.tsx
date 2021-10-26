d4b2121f7371db85681e7a8add9ea1dc7ca3a1c1
jakobw10 <jakobw@udel.edu>
2021-10-26 09:56:50 -0400

    Clear courses button works

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Course from "../Interfaces/Course";
 
 export function Semester(props:{year: number, season: string, courses: Course[]}): JSX.Element{
-    const courseList=props.courses.map((Course)=><li key={Course.name}><p>{Course.name}   Credits:{Course.credits}</p></li>);
+    const [courses,setCourses]=useState(props.courses);
+    const courseList=courses.map((Course)=><li key={Course.name}><p>{Course.name}   Credits:{Course.credits}</p></li>);
+    function clearSemester(){
+        setCourses([]);
+    }
     return <div className={"semester"}>
         <div><h5>Year {props.year} {props.season} Semester </h5></div>
         <br/>
-        <ul>{courseList}</ul>
+        <ul>{courseList} <button onClick={()=>clearSemester()}>Clear courses</button></ul>
     </div>
     ;
 }
