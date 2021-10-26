d35ae17299e21c23fa61bd972b2a045ed5080f58
jakobw10 <jakobw@udel.edu>
2021-10-26 09:29:22 -0400

    Semester component uses map to display courses

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Course from "../Interfaces/Course";
 
 export function Semester(props:{year: number, season: string, courses: Course[]}): JSX.Element{
+    const courseList=props.courses.map((Course)=><li key={Course.name}><p>{Course.name}   Credits:{Course.credits}</p></li>);
     return <div className={"semester"}>
         <div><h5>Year {props.year} {props.season} Semester </h5></div>
         <br/>
-        <p>{props.courses[0].name}    Credits: {props.courses[0].credits}</p>
-        <p>{props.courses[1].name}    Credits: {props.courses[1].credits}</p>
+        <ul>{courseList}</ul>
     </div>
     ;
 }
