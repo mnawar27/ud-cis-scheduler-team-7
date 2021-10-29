60ec965f44d81ebc321c3e83e9003741fb432f88
Maisha Nawar <maishanawar27@gmail.com>
2021-10-29 12:07:52 -0400

    adding edit button

--- src/Components/AddCourseMenu.tsx
+++ src/Components/AddCourseMenu.tsx
@@ -23,3 +23,4 @@ export function AddCourseMenu(props:{trigger:boolean;courses:Course[] ; setTrigg
         </div>;
     
 }
+
--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import { Button } from "react-bootstrap";
 import COURSES from "../Assets/courses.json";
 
 export function Menu(): JSX.Element{
+    
     return(
         <div className="menu">
             <h3>Course list</h3>
             <h6>Incomplete Requirements</h6>
             <ul className="nav navbar-nav">
                 {COURSES.map((Course, i) => {
-                    return <li key={i}><strong>{Course.name}</strong> {Course.description}</li>;
+                    const[edit, setEdit] = useState(COURSES[i]["description"]);
+                    function handleOnClick(newName: string) {
+                        //have some type of input field that modifies newName
+                        //enter removes input field
+                        //then we call the line below
+                        setEdit(newName);
+                    }
+                  
+                    return (
+                    
+                        <li key={i}><strong >{Course.name}</strong> {Course.description} 
+                            <li><Button className="btn btn-light btn-sm"> Edit </Button></li>
+                        </li>
+                    );
                 })}
             </ul>
             <hr/>
