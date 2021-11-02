5848f8a0f794ee218c0b6bfb1519e2f6a4a5b738
Maisha Nawar <maishanawar27@gmail.com>
2021-11-02 17:48:20 -0400

    fixing edit

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -3,21 +3,32 @@ import { Button, Form } from "react-bootstrap";
 import COURSES from "../Assets/courses.json";
  
 export function Menu(): JSX.Element{
+
+    function saveEdit() {
+        //update classes at all other parts of website
+        
+        console.log("hi");
+    }
+
     return(
         <div className="menu">
             <h3>Course list</h3>
             <h6>Incomplete Requirements</h6>
             <ul className="nav navbar-nav">
                 {COURSES.map((Course, i) => {
-                    const[edit, setEdit] = useState(true);
+                    const[edit, setEdit] = useState(COURSES[i]["description"]);
                     return (
                         <div key={i}>
                             <Form.Group>
-                                <Form.Label><strong>{Course.name}</strong></Form.Label>
-                                <Form.Control defaultValue={Course.description} disabled={edit}/>
+                                <Form.Label><strong>{Course.name} </strong></Form.Label>
+                                <Form.Label> {edit}</Form.Label>
+                                <Form.Control 
+                                    value={edit}
+                                    onChange={(ev:React.ChangeEvent<HTMLTextAreaElement>) => setEdit(ev.target.value)}/>
                             </Form.Group>
-                            <Button onClick={()=>setEdit(false)}>Click to Edit</Button>
-                            <Button disabled={edit} onClick={()=>setEdit(true)}>Submit</Button>
+                            <Button onClick={()=>setEdit("")}>Click to Edit</Button>
+                            <Button onClick={saveEdit}>Submit</Button>
+    
                         </div>
                     );
                 })}
