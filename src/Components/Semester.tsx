55a5ff3ac7c9131e46f220e6e808e43a0e387540
Maisha Nawar <maishanawar27@gmail.com>
2021-11-04 20:52:15 -0400

    Edit button works!

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -3,10 +3,14 @@ import { Button } from "react-bootstrap";
 import Course from "../Interfaces/Course";
 import {AddCourseMenu} from "./AddCourseMenu";
 import COURSES from "../Assets/courses.json";
+import EditCourse from "./EditCourse";
 
 export function Semester(props:{courses:Course[];year:number;season:string;setCurrentCourses:(arg0:Course[])=>void}): JSX.Element{
     const [popup,setPopup]=useState(false);
     const [courses,setCourses]=useState(props.courses);
+
+    const [editDiagram, setEditDiagram] = useState(false);
+    const [editTmpId,setEditTmpId] = useState<number>(0);
 
     function clearSemester(){
         setCourses([]);
@@ -21,8 +25,30 @@ export function Semester(props:{courses:Course[];year:number;season:string;setCu
         }
         setCourses(copy1);
     }
-    
 
+    const editCourse = (id:any) => {
+        setEditDiagram(true);
+        const tmpCourse = courses.filter((res)=>{
+            return res.id == id;
+        });
+        setEditTmpId(tmpCourse[0].id);
+    };
+  
+    const editAddCourse=(tmpCourse:any)=>{
+        let curIndex = 0;
+        const curCourse = JSON.parse(JSON.stringify(courses));
+        courses.forEach((course,index) => {
+            if (course.id == tmpCourse.id) curIndex = index;
+        });
+        curCourse[curIndex] = tmpCourse;
+        setCourses(curCourse);
+        setEditDiagram(false);
+    };
+  
+    const cancelEditDiagram = () => {
+        setEditDiagram(false);
+    };
+ 
     return <div className={"semester"}>
         Year {props.year}   {props.season} Semester
         <table>
@@ -52,6 +78,7 @@ export function Semester(props:{courses:Course[];year:number;season:string;setCu
                         }
                         )}</td>
                         <td scope="col"><button className="btn btn-light btn-sm" onClick={()=>removeCourse(Course)}>Remove course</button></td>
+                        <td scope="col"><button className="btn btn-light btn-sm" onClick={()=>editCourse(Course.id)}>Edit course</button></td>
 
                     </tr>
                 );
@@ -60,6 +87,17 @@ export function Semester(props:{courses:Course[];year:number;season:string;setCu
         <Button className="btn btn-light btn-sm" onClick={()=>clearSemester()}>Clear courses</Button>
         <Button className="btn btn-light btn-sm" onClick={()=>setPopup(true)}>Add course</Button>
         <AddCourseMenu trigger={popup} setTrigger={setPopup} setCourses={setCourses} courses={courses}></AddCourseMenu>
+
+        {editDiagram?
+            <div className='outer-diagram'>
+                <div className='diagram'>
+                    <EditCourse  editTmpId={editTmpId}  editAddCourse={editAddCourse}/>
+                    <button className='diagram-cancel btn btn-primary' onClick={cancelEditDiagram}>Cancel</button>
+                </div>
+            </div> :
+            <div></div>
+        }
+
     </div>
     ;
 }
