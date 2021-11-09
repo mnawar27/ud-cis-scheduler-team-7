4a5df253ebde931d0612b4ff295d9a71c67cd3e6
jakobw10 <jakobw@udel.edu>
2021-11-09 09:58:51 -0500

    Can press button to change course info then a seperate button to send the changes to the semesters

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import COURSES from "../Assets/courses.json";
 
+
 export function Menu(): JSX.Element{
+    function overrideCourse(i:number){
+        COURSES[i].name="CISC0";
+        
+    }
     return(
         <div className="menu">
             <h3>Course list</h3>
             <h6>Incomplete Requirements</h6>
             <ul className="nav navbar-nav">
                 {COURSES.map((Course, i) => {
-                    return <li key={i}><strong>{Course.name}</strong> {Course.description}</li>;
+                    return <li key={i}><strong>{Course.name}</strong> {Course.description} <button onClick={()=>overrideCourse(i)}></button></li>;
                 })}
             </ul>
             <hr/>
