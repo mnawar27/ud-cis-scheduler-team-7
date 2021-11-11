8336ac89d98a8b56b098e80fc49a5872500242e5
jakobw10 <jakobw@udel.edu>
2021-11-11 10:33:11 -0500

    removal works but broke default button

--- src/App.tsx
+++ src/App.tsx
@@ -15,29 +15,30 @@ import "./Components/Semester.css";
 import "./Components/Menu.css";
 function App(): JSX.Element {
     const [currentCourses, setCurrentCourses] = useState([COURSES[0],COURSES[1],COURSES[2],COURSES[3]]as Course[]);
-    const [fallsemesters, setFallSemesters]=useState([1,2,3,4]);
+    const [fallsemesters, setFallSemesters]=useState([currentCourses,currentCourses,currentCourses,currentCourses]);
     const [springsemesters, setSpringSemesters]=useState([1,2,3,4]);
     function useForceUpdate(){
         const [value,setValue] = useState(0); // integer state
         value;
         return () => setValue(value => value + 1); // update the state to force render
     }
-    function addFallSemester(semesters: number[]){
-        const i: number = semesters.length;
-        setFallSemesters([...semesters,i+1]);
+    function addFallSemester(semesters: Course[][]){
+        setFallSemesters([...semesters,currentCourses]);
     }
     function addSpringSemester(semesters: number[]){
         const i: number = semesters.length;
         setSpringSemesters([...semesters,i+1]);
     }
-    function removeFallSemester(semesters: number[]){
+    function removeFallSemester(semesters: Course[][]){
         let n:number;
-        const copy: number[] = [...semesters];
+        const copy: Course[][] = [...semesters];
         copy.splice(semesters.length-1,1);
-        for(let i=0;i<currentCourses.length;i++){
-            n=currentCourses[i].id;
-            COURSES[n-1].enrolled=false;
-        }
+        if(semesters.length>0){
+            for(let i=0;i<semesters.length;i++){
+                n=semesters[i][semesters.length-1].id;
+                COURSES[n-1].enrolled=false;
+            }
+        }   
         setFallSemesters(copy);
     }
     function removeSpringSemester(semesters: number[]){
@@ -45,8 +46,8 @@ function App(): JSX.Element {
         copy.splice(semesters.length-1,1);
         setSpringSemesters(copy);
     }
-    function clearSemester(fsemesters: number[], ssemesters: number[]){
-        const fcopy: number[] = [...fsemesters];
+    function clearSemester(fsemesters: Course[][], ssemesters: number[]){
+        const fcopy: Course[][] = [...fsemesters];
         const scopy: number[] = [...ssemesters];
         fcopy.splice(0,fsemesters.length);
         scopy.splice(0,ssemesters.length);
@@ -57,7 +58,7 @@ function App(): JSX.Element {
         setSpringSemesters(scopy);
     }
     function setDefault(){
-        setFallSemesters([1,2,3,4]);
+        setFallSemesters([currentCourses,currentCourses,currentCourses,currentCourses]);
         setSpringSemesters([1,2,3,4]);
     }
     return (
@@ -81,10 +82,10 @@ function App(): JSX.Element {
                         </div>
                         <button className="btn btn-light btn-sm" onClick={()=>addFallSemester(fallsemesters)}>Add fall semester</button>
                         <button className="btn btn-light btn-sm" onClick={()=>removeFallSemester(fallsemesters)}>Remove last fall semester</button>
-                        {fallsemesters.map((i)=>{
+                        {fallsemesters.map((Courses,i)=>{
                             return(
-                                <Semester key = {i}
-                                    year = {i}
+                                <Semester key = {null}
+                                    year={i+1}
                                     season = {"Fall"} 
                                     courses = {currentCourses}
                                     setCurrentCourses={setCurrentCourses}/>);
